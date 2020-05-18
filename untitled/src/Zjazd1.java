bf58c88e540a35355b3ad1027b15e33e590226e1
s20598-pj <56222968+s20598-pj@users.noreply.github.com>
2020-05-18 18:45:17 +0200

    coś

--- untitled/src/Zjazd1.java
+++
@@ -1,7 +0,0 @@
-public class Zjazd1 {
-
-    int a = 100;
-    int b = 222;
-
-
-}
--- untitled/src/Zjazd2.java
+++
@@ -1,7 +0,0 @@
-public class Zjazd2 {
-
-    public static void main(String[] args){
-        ModelS acid = new ModelS(1, 2,3,4,5,6,7);
-    }
-
-}
